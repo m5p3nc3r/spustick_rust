3d3576379be4861c0becafc15ef4ae2110474d2d
Matt Spencer <matthew@thespencers.me.uk>
2022-05-25 22:02:38 +0100

    Extract HID code into module
    Signed-off-by: Matt Spencer <matthew@thespencers.me.uk>

---
+++ src/hid.rs
@@ -0,0 +1,91 @@
+use bsp::hal::pac::interrupt;
+use bsp::{
+    hal::{self, clocks::UsbClock, pac},
+    pac::{RESETS, USBCTRL_DPRAM, USBCTRL_REGS},
+};
+use rp_pico as bsp;
+
+use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
+use usb_device::{class_prelude::UsbBusAllocator, device::UsbDevice};
+use usbd_hid::descriptor::generator_prelude::*;
+use usbd_hid::hid_class::HIDClass;
+
+use crate::joystick::SpustickReport;
+
+/// The USB Device Driver (shared with the interrupt).
+static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
+
+/// The USB Bus Driver (shared with the interrupt).
+static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+
+/// The USB Human Interface Device Driver (shared with the interrupt).
+static mut USB_HID: Option<HIDClass<hal::usb::UsbBus>> = None;
+
+// Initialise the USB HID subsystem
+pub fn init_hid(
+    ctrl_regs: USBCTRL_REGS,
+    ctrl_dpram: USBCTRL_DPRAM,
+    resets: &mut RESETS,
+    usb_clock: UsbClock,
+) {
+    // Set up the USB driver
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        ctrl_regs, ctrl_dpram, usb_clock, true, resets,
+    ));
+    unsafe {
+        // Note (safety): This is safe as interrupts haven't been started yet
+        USB_BUS = Some(usb_bus);
+    }
+
+    // Grab a reference to the USB Bus allocator. We are promising to the
+    // compiler not to take mutable access to this global variable whilst this
+    // reference exists!
+    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
+
+    // Set up the USB HID Class Device driver, providing Mouse Reports
+    let usb_hid = HIDClass::new(bus_ref, SpustickReport::desc(), 60);
+    unsafe {
+        // Note (safety): This is safe as interrupts haven't been started yet.
+        USB_HID = Some(usb_hid);
+    }
+
+    // Create a USB device with a fake VID and PID
+    let usb_dev = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0x16c0, 0x27da))
+        .manufacturer("Fake company")
+        .product("Twitchy Mousey")
+        .serial_number("TEST")
+        .device_class(0) // misc
+        .build();
+    unsafe {
+        // Note (safety): This is safe as interrupts haven't been started yet
+        USB_DEVICE = Some(usb_dev);
+    }
+
+    unsafe {
+        // Enable the USB interrupt
+        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    };
+}
+
+/// This function is called whenever the USB Hardware generates an Interrupt
+/// Request.
+#[allow(non_snake_case)]
+#[interrupt]
+unsafe fn USBCTRL_IRQ() {
+    // Handle USB request
+    let usb_dev = USB_DEVICE.as_mut().unwrap();
+    let usb_hid = USB_HID.as_mut().unwrap();
+    usb_dev.poll(&mut [usb_hid]);
+}
+
+/// Submit a new mouse movement report to the USB stack.
+///
+/// We do this with interrupts disabled, to avoid a race hazard with the USB IRQ.
+pub fn push_mouse_movement(report: SpustickReport) -> Result<usize, usb_device::UsbError> {
+    cortex_m::interrupt::free(|_| unsafe {
+        // Now interrupts are disabled, grab the global variable and, if
+        // available, send it a HID report
+        USB_HID.as_mut().map(|hid| hid.push_input(&report))
+    })
+    .unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![no_std]
 #![no_main]
 
+mod hid;
 mod joystick;
 
 use cortex_m_rt::entry;
@@ -15,29 +16,13 @@ use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use bsp::hal;
+use bsp::hal::{self};
 use rp_pico as bsp;
-use rp_pico::hal::pac::interrupt;
 
-use bsp::hal::{clocks::Clock, pac};
-use usb_device::{
-    class_prelude::UsbBusAllocator,
-    device::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
-};
-// USB Human Interface Device (HID) Class support
-use usbd_hid::descriptor::generator_prelude::*;
-use usbd_hid::hid_class::HIDClass;
+use bsp::hal::clocks::Clock;
+use bsp::hal::pac;
 
-use crate::joystick::SpustickReport;
-
-/// The USB Device Driver (shared with the interrupt).
-static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
-
-/// The USB Bus Driver (shared with the interrupt).
-static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
-
-/// The USB Human Interface Device Driver (shared with the interrupt).
-static mut USB_HID: Option<HIDClass<hal::usb::UsbBus>> = None;
+use crate::{hid::init_hid, joystick::SpustickReport};
 
 #[entry]
 fn main() -> ! {
@@ -63,47 +48,13 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    // Set up the USB driver
-    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+    init_hid(
         pac.USBCTRL_REGS,
         pac.USBCTRL_DPRAM,
-        clocks.usb_clock,
-        true,
         &mut pac.RESETS,
-    ));
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet
-        USB_BUS = Some(usb_bus);
-    }
+        clocks.usb_clock,
+    );
 
-    // Grab a reference to the USB Bus allocator. We are promising to the
-    // compiler not to take mutable access to this global variable whilst this
-    // reference exists!
-    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
-
-    // Set up the USB HID Class Device driver, providing Mouse Reports
-    let usb_hid = HIDClass::new(bus_ref, SpustickReport::desc(), 60);
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet.
-        USB_HID = Some(usb_hid);
-    }
-
-    // Create a USB device with a fake VID and PID
-    let usb_dev = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0x16c0, 0x27da))
-        .manufacturer("Fake company")
-        .product("Twitchy Mousey")
-        .serial_number("TEST")
-        .device_class(0) // misc
-        .build();
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet
-        USB_DEVICE = Some(usb_dev);
-    }
-
-    unsafe {
-        // Enable the USB interrupt
-        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
-    };
     let core = pac::CorePeripherals::take().unwrap();
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
@@ -116,7 +67,7 @@ fn main() -> ! {
             y: 50,
             buttons: 0,
         };
-        push_mouse_movement(rep_up).ok().unwrap_or(0);
+        hid::push_mouse_movement(rep_up).ok().unwrap_or(0);
 
         delay.delay_ms(1000);
 
@@ -125,31 +76,8 @@ fn main() -> ! {
             y: -100,
             buttons: 0xf,
         };
-        push_mouse_movement(rep_down).ok().unwrap_or(0);
+        hid::push_mouse_movement(rep_down).ok().unwrap_or(0);
     }
-}
-
-/// Submit a new mouse movement report to the USB stack.
-///
-/// We do this with interrupts disabled, to avoid a race hazard with the USB IRQ.
-fn push_mouse_movement(report: joystick::SpustickReport) -> Result<usize, usb_device::UsbError> {
-    cortex_m::interrupt::free(|_| unsafe {
-        // Now interrupts are disabled, grab the global variable and, if
-        // available, send it a HID report
-        USB_HID.as_mut().map(|hid| hid.push_input(&report))
-    })
-    .unwrap()
-}
-
-/// This function is called whenever the USB Hardware generates an Interrupt
-/// Request.
-#[allow(non_snake_case)]
-#[interrupt]
-unsafe fn USBCTRL_IRQ() {
-    // Handle USB request
-    let usb_dev = USB_DEVICE.as_mut().unwrap();
-    let usb_hid = USB_HID.as_mut().unwrap();
-    usb_dev.poll(&mut [usb_hid]);
 }
 
 // End of file
